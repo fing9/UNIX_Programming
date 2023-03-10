44d4eb8d4cbbebbae778ca7aaa2b2b0286f4e8e1
fing9 <djajsl54@naver.com>
2023-03-10 09:53:25 +0900

    bugfix : 쉘의 세그먼트 폴트 에러

--- .vimrc
+++ .vimrc
@@ -1,5 +1,1 @@
-set nu
-set si
-set sts=4
-set ts=4
-set shiftwidth=4
+
---
+++ LABsocket/socket.c
@@ -0,0 +1,30 @@
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<dirent.h>
+#include<ftw.h>
+#include<time.h>
+#include<signal.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<sys/stat.h>
+#include<sys/mman.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<sys/sem.h>
+#include<sys/shm.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+
+#define SOCKET_NAME "wksocket"
+
+// 잘 알려진 포트 번호를 입력받아 이에 해당하는 서비스명을 출력하는 프로그램을 작성하라.
+
+int main(void) {
+	
+
+	exit(0);
+}
+
